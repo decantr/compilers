d082f5f48075fb136c85ced8aa7a9b19a20f9203
decantr <theeasterlywood@gmail.com>
2018-10-15 21:54:37 +0100

    starting point for lab 3

---
+++ third/Triangle.Compiler/SyntacticAnalyzer/SourcePosition.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Triangle.Compiler.SyntacticAnalyzer
+{
+    /// <summary>
+    /// A range of locations in a file
+    /// </summary>
+    public class SourcePosition
+    {
+        /// <summary>
+        /// Creates a new file range location
+        /// </summary>
+        /// <param name="startLocation">The starting location of the range</param>
+        /// <param name="endLocation">The ending location of the range</param>
+        public SourcePosition(Location startLocation, Location endLocation)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
