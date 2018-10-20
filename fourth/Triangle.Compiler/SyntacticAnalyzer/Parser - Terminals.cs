8fd20df92ab214faff1ba3af32fae8511ec7e3f8
decantr <theeasterlywood@gmail.com>
2018-10-20 22:13:02 +0100

    complete task 5

--- fourth/Triangle.Compiler/SyntacticAnalyzer/Parser - Terminals.cs
+++ fourth/Triangle.Compiler/SyntacticAnalyzer/Parser - Terminals.cs
@@ -19,5 +19,20 @@ namespace Triangle.Compiler.SyntacticAnalyzer {
 			System.Console.WriteLine( "parsing identifier" );
 			Accept(TokenKind.Identifier);
 		}
+
+		void ParseOperator() {
+			System.Console.WriteLine( "parsing operator" );
+			Accept( TokenKind.Operator );
+		}
+
+		void ParseIntLiteral() {
+			System.Console.WriteLine( "parsing integer literal" );
+			Accept( TokenKind.IntLiteral );
+		}
+
+		void ParseCharLiteral() {
+			System.Console.WriteLine( "parsing character literal" );
+			Accept( TokenKind.CharLiteral );
+		}
 	}
 }
