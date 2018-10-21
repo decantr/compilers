cbd90ee5945bef09537ba96a159a02572f12fd70
decantr <theeasterlywood@gmail.com>
2018-10-21 01:22:02 +0100

    implement code given for location and source pos

--- fifth/Triangle.Compiler/SyntacticAnalyzer/Location.cs
+++ fifth/Triangle.Compiler/SyntacticAnalyzer/Location.cs
@@ -1,9 +1,19 @@
-﻿namespace Triangle.Compiler.SyntacticAnalyzer
-{
+﻿namespace Triangle.Compiler.SyntacticAnalyzer {
     /// <summary>
     /// A location in a file
     /// </summary>
-    public class Location
-    {
+    public class Location {
+		public int Line { get; }
+
+		public int Column { get; }
+
+		public Location( int line, int column ) {
+			Line = line;
+			Column = column;
+		}
+
+		public override string ToString() {
+			return $"Line {Line}, Column {Column}";
+		}
     }
 }
