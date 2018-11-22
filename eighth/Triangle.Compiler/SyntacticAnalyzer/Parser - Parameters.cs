5bff442fddd13123a3f2b92650441538f228433c
decantr <theeasterlywood@gmail.com>
2018-11-22 17:21:43 +0000

    fix issue in parser

--- eighth/Triangle.Compiler/SyntacticAnalyzer/Parser - Parameters.cs
+++ eighth/Triangle.Compiler/SyntacticAnalyzer/Parser - Parameters.cs
@@ -17,12 +17,14 @@ namespace Triangle.Compiler.SyntacticAnalyzer
 		{
 			Compiler.WriteDebuggingInfo("Parsing Parameters");
 			Location startLocation = tokens.Current.Position.Start;
-			Parameter p = ParseParameter();
 			if ( tokens.Current.Kind == TokenKind.RightBracket ) {
-				SourcePosition sourcePosition = new SourcePosition(startLocation, tokens.Current.Position.Finish);
-				return new EmptyParameterSequence( sourcePosition );
+				SourcePosition parameterPosition = new SourcePosition(startLocation, tokens.Current.Position.Finish);
+				return new EmptyParameterSequence( parameterPosition );
 			}
-			else if (tokens.Current.Kind == TokenKind.Comma)
+
+			Parameter p = ParseParameter();
+
+			if (tokens.Current.Kind == TokenKind.Comma)
 			{
 				AcceptIt();
 				ParameterSequence p2 = ParseParameters();
